2357b67e765e3692dfdb4dcae1be6b4b59eb239c
Theresa-o <62052443+Theresa-o@users.noreply.github.com>
2023-10-24 21:45:08 -0700

    Refactor serializer logic to set data validation

--- icecreamtruck/icecreamapi/serializers.py
+++ icecreamtruck/icecreamapi/serializers.py
@@ -1,48 +1,88 @@
-# from rest_framework import serializers
-# from .models import FoodItem, FoodFlavor, Customer, Inventory, Transaction, Earnings
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import FoodItem, FoodFlavor, Truck, Sale
 
-# class FoodFlavorSerializer(serializers.ModelSerializer):
-#     """
-#     Serializer for the FoodFlavor model.
+class FoodFlavorSerializer(serializers.ModelSerializer):
+    """
+    This serializer is used for serializing food item flavors.
+    """
 
-#     This serializer is used for serializing food item flavors.
+    class Meta:
+        model = FoodFlavor
+        fields = "__all__"
 
-#     Fields:
-#         All fields from the FoodFlavor model.
-#     """
-
-#     class Meta:
-#         model = FoodFlavor
-#         fields = "__all__"
-
-# class FoodItemSerializer(serializers.ModelSerializer):
-#     """
-#     Serializer for the FoodItem model.
-
-#     This serializer is used for serializing food items and their associated flavors.
-
-#     Fields:
-#         All fields from the FoodItem model.
-#     """
-#     foodflavor_set = FoodFlavorSerializer(many=True, read_only=True)
+class FoodItemSerializer(serializers.ModelSerializer):
+    """
+    This serializer is used for serializing food items and their associated flavors.
+    """
     
-#     class Meta:
-#         model = FoodItem
-#         fields = "__all__"
+    class Meta:
+        model = FoodItem
+        fields = ["name", "price", "quantity", "item_type", "image"]
 
-# class CustomerSerializer(serializers.ModelSerializer):
-#     """
-#     Serializer for the Customer model.
+class UserSerializer(serializers.ModelSerializer):
+    """
+    This serializer is used for serializing customer data.
+    """
 
-#     This serializer is used for serializing customer data.
+    class Meta:
+        model = User
+        fields = ["username", "email"]
 
-#     Fields:
-#         All fields from the Customer model.
-#     """
+class TruckSerializer(serializers.ModelSerializer):
+    """
+    Serializer for truck model.
+    """
 
-#     class Meta:
-#         model = Customer
-#         fields = "__all__"
+    total_sales = serializers.SerializerMethodField()
+    food_items = FoodItemSerializer(many=True)
+
+    class Meta:
+        model = Truck
+        fields = ["id", "name", "food_items", "total_sales"]
+
+    def get_total_sales(self, obj):
+        return obj.total_sales()
+
+class PurchaseSerializer(serializers.Serializer):
+
+    food_id = serializers.IntegerField()
+    quantity = serializers.IntegerField()
+
+    def validate_quantity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Quantity must be a positive integer")
+        return value
+    
+class CreateTruckSerializer(serializers.Serializer):
+    # creating a new truck 
+    name = serializers.CharField(max_length=100, required=True)
+
+class CreateFoodItemSerializer(serializers.ModelSerializer):
+    # add a field for flavour
+    # flavor = serializers.CharField(max_length=2, required=True)
+    food_flavor = serializers.CharField(max_length=2)
+
+    class Meta:
+        model = FoodItem
+        fields = ["name", "price", "quantity", "item_type", "flavor"]
+
+    def validate_item_type(self, value):
+        # validate the food item type field to ensure that it matches with the choices in the model
+        valid_choices = [choice[0] for choice in FoodItem.ITEM_TYPES]
+
+        if value not in valid_choices:
+            raise serializers.ValidationError(f"'item_type' must be one of {valid_choices}")
+
+    def validate_food_flavor(self, value):
+        # validate the food flavor field to ensure that it matches with the choices in the model
+        valid_choices = [choice[0] for choice in FoodFlavor.FLAVORS]
+
+        if value not in valid_choices:
+            raise serializers.ValidationError(f"'food_flavor' must be one of {valid_choices}")
+        return value
+    
+
 
 # class InventorySerializer(serializers.ModelSerializer):
 #     """
